dd9abd9d18886e6d6481a3cbf0f8cc74556a7710
TerminDev <dev.demystify213@passinbox.com>
2025-05-18 21:53:21 +0300

    some fixes

--- src/components/AboutMe.svelte
+++ src/components/AboutMe.svelte
@@ -1,8 +1,8 @@
 <section class="py-24 px-6 text-base-content text-center" id="about">
   <div class="z-10 max-w-5xl mx-auto text-2xl md:text-3xl my-4">
-    <h2 class="text-4xl font-bold text-secondary mb-2">About me</h2>
-    <h2 class="text-2xl font-bold mb-4">
-      A brief <span class="text-primary">introduction</span> about me.
+    <h2 class="text-4xl font-bold text-primary mb-2">About me</h2>
+    <h2 class="text-2xl mb-6">
+      A brief <span class="text-secondary">introduction</span> about me.
     </h2>
 
     <p>
@@ -16,7 +16,7 @@
       <span class="font-bold text-accent">hacker</span> at night.
     </p>
 
-    <p class="text-base-content/90 text-xl md:text-2xl my-4">
+    <!-- <p class="text-base-content/90 text-xl md:text-2xl my-4">
       Well, other than that, I like
       <span class="text-accent"> reading</span>,
       <span class="text-accent"> studying</span>, and
@@ -24,13 +24,13 @@
         turning whatever comes to my field of imaginations to reality with the
         help of code</span
       >.
-    </p>
-
+    </p> -->
+    <!-- 
     <p class="text-xl md:text-2xl my-4">
       I like <span class="text-info">thinking for long periods of time</span>,
       sometimes I’d think about a problem for weeks, I’d watch courses, read
       books — all just to get the satisfaction of solving a problem.
-    </p>
+    </p> -->
 
     <p class="text-xl md:text-2xl my-4">
       I’m also fluent in three languages! I speak
